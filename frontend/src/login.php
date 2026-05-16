c8c80604fbc096923cbd319273dea60556cf1b28
BrMedina <medina.brent26@gmail.com>
2026-05-17 02:18:13 +0800

    Add Bootstrap Icons and enhance input/button styles for better UI

--- frontend/src/login.php
+++ frontend/src/login.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="style.css">
     <title>Login</title>
 </head>
@@ -20,9 +21,11 @@
                     <div class="card-body p-5">
                         <h2 class="mb-4 text-center fw-bold mt-2">Login</h2>
 
+                        <i class="bi bi-envelope"></i>
                         <label for="email" class="fw-bold">Email</label>
                         <input type="email" name="email" id="email" placeholder="johndoe@gmail.com" required class="form-control">
 
+                        <i class="bi bi-lock"></i>
                         <label for="password" class="fw-bold mt-3">Password</label>
                         <input type="password" name="password" id="password" placeholder="••••••••" required class="form-control">
 
@@ -31,7 +34,7 @@
                         </div>
 
                         <div class="mt-3">
-                            <button type="submit" class="btn btn-primary btn-lg w-100" onclick="">Login</button>
+                            <a href="index.php"><button type="submit" class="btn btn-primary btn-lg w-100 button">Login</button></a>
                             <hr>
                         </div>
                         <div class="d-flex justify-content-center mt-1">
--- frontend/src/register.php
+++ frontend/src/register.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="style.css">
     <title>Register</title>
 </head>
@@ -22,27 +23,31 @@
 
                         <div class="row">
                             <div class="col mb-3">
+                                
+                                <i class="bi bi-person"></i>
                                 <label for="" class="fw-bold">Username</label>
                                 <input type="text" name="username" id="username" placeholder="John Doe" required class="form-control">
                             </div>
                             <div class="col">
+                                <i class="bi bi-telephone"></i>
                                 <label for="" class="fw-bold">Contact Number</label>
                                 <input type="tel" name="number" id="number" placeholder="09XXXXXXX" required class="form-control">
                             </div>
                         </div>
                         
                         
-
+                        <i class="bi bi-envelope"></i>
                         <label for="email" class="fw-bold">Email</label>
-                        <input type="email" name="email" id="email" placeholder="johndoe@gmail.com" required class="form-control">
+                        <input type="email" name="email" id="email" placeholder="johndoe@gmail.com" required class="form-control bi bi-envelope">
 
+                        <i class="bi bi-lock"></i>
                         <label for="password" class="fw-bold mt-3">Password</label>
                         <input type="password" name="password" id="password" placeholder="••••••••" required class="form-control">
 
 
 
                         <div class="mt-3">
-                            <button type="submit" class="btn btn-primary btn-lg w-100">Register</button>
+                            <button type="submit" class="btn btn-primary btn-lg w-100 button">Register</button>
                             <hr>
                         </div>
                         <div class="d-flex justify-content-center mt-1">
