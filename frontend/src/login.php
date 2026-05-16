8df28367f6d6f5918803f64934c2d74738aa2be7
BrMedina <medina.brent26@gmail.com>
2026-05-16 17:48:37 +0800

    Refactor code structure for improved readability and maintainability

--- frontend/src/login.php
+++ frontend/src/login.php
@@ -13,17 +13,33 @@
         <div class="row g-0 justify-content-center">
             <div class="col-lg-6 d-none d-lg-block">
                 <div class="d-flex flex-column justify-content-center h-100">
-                    <div class="d-flex justify-content-center">
-                        <img src="../assets/LibraryLogo.png" alt="LibLogo" height="300" width="300">
+                    <div class="d-flex justify-contnet-center">
+                        <img src="../assets/logo2.png" alt="LibLogo" width="800px" height="200px">
                     </div>
-                    <div class="login-text">UNIVERSITY OF SANTO TOMAS</div>
-                    <div class="login-text-sub">MIGUEL DE BENAVIDES LIBRARY</div>
                 </div>
             </div>
 
             <div class="col-lg-6 col-md-7 col-12">
                 <div class="card shadow">
                     <div class="card-body p5">
+                        <h2 class="mb-4 text-center fw-bold mt-3">Login</h2>
+
+                        <label for="" class="fw-bold">Email</label>
+                        <input type="email" name="email" id="email" placeholder="johndoe@gmail.com" required class="form-control">
+
+                        <label for="" class="fw-bold mt-3">Password</label>
+                        <input type="password" name="password" id="password" placeholder="••••••••" required class="form-control">
+
+                        <div class="row">
+                            <button class="forgot-password mt-2">Forgot Password?</button>
+                        </div>
+                        
+
+                        <div class="row px-3 mt-3">
+                            <button class="btn btn-primary btn-lg btn-block">Login</button>
+                        </div>
+
+
 
                     </div>
                 </div>
