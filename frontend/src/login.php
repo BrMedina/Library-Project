57c7d6e7918e513187a3caeca80bc94103cbef23
BrMedina <medina.brent26@gmail.com>
2026-05-16 16:15:10 +0800

    Add initial frontend files for library application
    - Created index.php for the main entry point of the application.
    - Added login.php with a responsive layout and Bootstrap styling.
    - Implemented register.php as a placeholder for user registration.
    - Introduced style.css for custom styles, including background image and typography.

---
+++ frontend/src/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+<body>
+
+    <div class="container-lg container-md py-5 d-flex justify-content-center flex-column h-100">
+        <div class="row g-0 justify-content-center">
+            <div class="col-lg-6 d-none d-lg-block">
+                <div class="d-flex flex-column justify-content-center h-100">
+                    <div class="d-flex justify-content-center">
+                        <img src="../assets/LibraryLogo.png" alt="LibLogo" height="300" width="300">
+                    </div>
+                    <div class="login-text">UNIVERSITY OF SANTO TOMAS</div>
+                    <div class="login-text-sub">MIGUEL DE BENAVIDES LIBRARY</div>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-7 col-12">
+                <div class="card shadow">
+                    <div class="card-body p5">
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
