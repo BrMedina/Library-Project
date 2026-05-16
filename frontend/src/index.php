25453c895b29d18677844ff41e86d3ff6f99cd25
Exterval <treblmao@gmail.com>
2026-05-16 17:56:04 +0800

    Add navbar and sidebar styles to enhance layout and navigation

--- frontend/src/index.php
+++ frontend/src/index.php
@@ -4,9 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Library</title>
 </head>
-<body>
-    
+<body style="background-color: #f7f7f7;">
+<!-- NAVBAR -->
+<nav class="navbar shadow-sm sidebar-navbar">
+  <div class="container-fluid d-flex flex-column align-items-start">
+    <a class="navbar-brand d-flex align-items-center gap-2 flex-wrap mb-3" href="#">
+      <img src="../assets/LibraryLogo.png" alt="Logo" width="50" height="44" class="d-inline-block align-text-top">
+      UST Miguel de Benavides <span class="text-primary">E-Library</span>
+    </a>
+    <div class="w-100" id="sidebarNav">
+      <ul class="navbar-nav flex-column w-100">
+        <li class="nav-item">
+          <a class="nav-link active py-2" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link py-2" href="#">About</a>
+        </li>
+      </ul>
+      <form class="d-flex mt-3 w-100" role="search">
+        <input class="form-control flex-grow-1 me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<!-- NAVBAR -->
+
+<!-- MAIN SECTION -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
